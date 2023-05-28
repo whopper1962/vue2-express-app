6866c90b0ab502ef7e825abff88ca43114739c19
Masashi Kawakami <dlold1962@gmail.com>
2023-05-28 21:09:51 +0900

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <home-view></home-view>
+  </div>
+</template>
+
+<script>
+import HomeView from './views/HomeView.vue'
+export default {
+  components: { HomeView },
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello",
+    };
+  },
+};
+</script>
